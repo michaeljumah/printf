b19899a088c9400400fe76aeabeccf091c9babc5
Kennedy <kenryan49@gmail.com>
2023-03-28 14:22:21 -0700

    Main header update

--- main.h
+++ main.h
@@ -1,9 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* inbuilt functions */
+#include <stdarg.h>
 
+
+/* print functions */
 int _printf(const char *format, ...);
-
+int printChar(va_list args, int printed);
+int printStr(va_list args, int printed);
+int printInt(va_list args, int printed);
+int printBinary(va_list args, int printed);
+int printUsign(va_list args, int printed);
+int printOctal(va_list args, int printed);
+int printHexLow(va_list args, int printed);
+int printHexUp(va_list args, int printed);
 
 
 #endif /* MAIN_H */
