9b3ecd9bc591a09cad7bfad556be4e2756a8e108
Kennedy <kenryan49@gmail.com>
2023-03-28 03:06:30 -0700

    printf function

--- _printf.c
+++ _printf.c
@@ -13,43 +13,41 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-
-	const char *p = format;
-	int chars_printed = 0;
+	int count = 0;
 
 	va_start(args, format);
-	while (*p != '\0')
+	while (*format)
 	{
-		if (*p == '%')
+		if (*format == '%')
 		{
-			++p;
-				if (*p == 'c')
-				{
-					char c = va_arg(args, int);
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
 
-					putchar(c);
-					++chars_printed;
-				}
-				else if (*p == 's')
-				{
-					char *str = va_arg(args, char *);
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
 
-					fputs(str, stdout);
-					chars_printed += strlen(str);
-				}
-				else if (*p == '%')
-				{
-					putchar('%');
-					++chars_printed;
-				}
+					fputs(s, stdout);
+					count += strlen(s);
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
 		}
 		else
 		{
-			putchar(*p);
-			++chars_printed;
+			putchar(*format);
+			count++;
 		}
-		++p;
+		format++;
 	}
 	va_end(args);
-	return (chars_printed);
+	return (count);
 }
